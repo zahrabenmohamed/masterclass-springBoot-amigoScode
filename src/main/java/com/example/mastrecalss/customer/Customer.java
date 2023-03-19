3488e95d76f0f890c8082fb56be44af43180d538
zahrabenmohamed <benmedzahra20@gmail.com>
2023-03-19 12:06:16 +0000

    update Entity

--- src/main/java/com/example/mastrecalss/customer/Customer.java
+++ src/main/java/com/example/mastrecalss/customer/Customer.java
@@ -1,11 +1,7 @@
 package com.example.mastrecalss.customer;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
